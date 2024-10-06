d8d37f77baf1facc1231705085aed4ac5408a910
amicoding <mdsagarkhan16656@gmail.com>
2024-10-06 16:14:00 +0600

    click event added for all button

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+let display = document.getElementById('display');
+
+let btns = document.querySelectorAll(".btns button");
+
+// console.log(btns)
+
+for (let i = 0; i < btns.length; i++) {
+  let btn = btns[i]
+  btn.addEventListener("click", function(e){
+    
+  let  text = e.target.innerText ;
+  
+    
+  } )
+}
+
